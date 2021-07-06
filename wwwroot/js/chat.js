1d8fed87bad1e8d04f2d78bdd0667fcf0fe04e3c
Ala Obeidat <ala-work@hotmail.com>
2021-07-06 16:16:33 +0300

    First Commit

---
+++ wwwroot/js/chat.js
@@ -0,0 +1,54 @@
+﻿ 
+var isConnected = false;
+var connection = new signalR.HubConnectionBuilder()
+    .configureLogging(signalR.LogLevel.Debug)
+    .withUrl("chatHub")
+    .withAutomaticReconnect([1000, 2000, 5000, 5000, 10000, 10000, 10000, 20000, 30000])
+    .build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage",  (user, message) =>{
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+connection.onreconnecting((error) => {
+    isConnected = false; 
+    var msg = error.message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+async function start() {
+    connection.start().then(function () {
+        isConnected = true;
+        document.getElementById("sendButton").disabled = false;
+    }).catch(function (err) {
+        isConnected = false;
+        return console.error(err.toString());
+    });
+}
+
+// show that we're reconnected
+connection.onreconnected((error) => {
+    isConnected = true;
+    var msg = error.message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+start();
